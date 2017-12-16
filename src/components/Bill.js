85da5e3e4e4d9c6d343dad6695c01104ad0cf88d
nutanek <nutcs@hotmail.com>
2017-12-17 01:03:41 +0700

    edit Bill component

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import PageLayout from './../layouts/PageLayout'
+import './../styles/Bill.css'
 
 const Bill = (props) => (
     <PageLayout>
-        <h1>
-            Hello, Bill
-        </h1>
+        <div className="row justify-content-center">
+            <div className="card col-lg-7 col-md-10 bill">
+                <div className="card-body">
+                    <Header title="Bill Calculator"/>
+                    
+                </div>
+            </div>
+        </div>
     </PageLayout>
+)
+
+const Header = (props) => (
+    <div className="row header justify-content-center">
+        {props.title}
+    </div>
 )
 
 export default Bill
